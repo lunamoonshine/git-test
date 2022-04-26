336ec209a91801f46d598e96d186b48484375b6e
luna <hello@luna.cn>
2022-04-26 19:15:37 +0800

    master commit1

--- src/main/java/giao.java
+++ src/main/java/giao.java
@@ -2,5 +2,6 @@ public class giao {
     public static void main(String[] args) {
         System.out.println("hll");
         System.out.println("dfdg2");
+        System.out.println("dfghusf");
     }
 }
